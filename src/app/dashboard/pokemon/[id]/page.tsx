5d0006d5c51e7d7d54436a4fff844237f1745d80
Leonardo Omaña <leotangram@gmail.com>
2023-07-24 21:37:20 -0500

    load pokemon id information

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,25 @@
+import { Pokemon } from '@/pokemons'
 import { FC } from 'react'
 interface PokemonPageProps {
   params: { id: string }
 }
 
-export default function PokemonPage({ params }: PokemonPageProps) {
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: 'force-cache' // TODO: cambiar esto en un futuro
+  }).then(response => response.json())
+  console.log('Se cargó: ', pokemon.name)
+
+  return pokemon
+}
+
+export default async function PokemonPage({ params }: PokemonPageProps) {
+  const pokemon = await getPokemon(params.id)
+
   return (
     <div>
       <h1>Pokémon {params.id}</h1>
+      <div>{JSON.stringify(pokemon)}</div>
     </div>
   )
 }
