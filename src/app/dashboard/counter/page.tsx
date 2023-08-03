3a3b2a3987ea2462e4b8323b7513ea38e28bf275
Leonardo Omaña <leotangram@gmail.com>
2023-08-03 16:30:44 -0500

    Counter Reducer actions

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,6 +1,6 @@
 
-import { CartCounter } from '@/shopping-cart'
 import { Metadata } from 'next'
+import { CartCounter } from '@/shopping-cart'
 
 export const metadata: Metadata = {
   title: 'Shopping Cart',
--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,29 +1,29 @@
 'use client'
-import { FC, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '@/store'
+import { addOne, substractOne } from '@/store/counter/counterSlice'
+import { FC } from 'react'
 
 interface CartCounterProps {
   value?: number
 }
 
 export const CartCounter: FC<CartCounterProps> = ({ value = 0 }) => {
-  const [counter, setCounter] = useState(value)
-
-  const updateCounter = (updater: number) =>
-    setCounter(counter => counter + updater)
+  const count = useAppSelector(state => state.counter.count)
+  const dispatch = useAppDispatch()
 
   return (
     <>
-      <span className="text-9xl">{counter}</span>
+      <span className="text-9xl">{count}</span>
       <div className="flex gap-2">
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px]"
-          onClick={() => updateCounter(-1)}
+          onClick={() => dispatch(substractOne())}
         >
           -1
         </button>
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px]"
-          onClick={() => updateCounter(+1)}
+          onClick={() => dispatch(addOne())}
         >
           +1
         </button>
